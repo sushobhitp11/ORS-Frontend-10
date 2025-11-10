3ac0c89a3c0440af579acbab85f38306af15dddb
sushobhitp11 <sushobhitp11@gmail.com>
2025-11-10 15:11:22 +0530

    Refactor login component for clarity and error handling
    Refactor login component by cleaning up code, improving readability, and adding input error handling.

--- ORSProject10-UI/src/app/login/login.component.ts
+++ ORSProject10-UI/src/app/login/login.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
 import { HttpServiceService } from '../http-service.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { DataValidator } from '../utility/data-validator';
 import { CookieService } from 'ngx-cookie-service';
-import { ActivatedRoute } from '@angular/router';
 import { ServiceLocatorService } from '../service-locator.service';
 
 @Component({
@@ -23,115 +21,88 @@ export class LoginComponent implements OnInit {
     loginId: '',
     password: '',
     loginUrl: '',
+    inputerror: {}               // ✅ Added inside form
   };
-
-  inputerror = {};
-  message = '';
 
   userparams = {
     url: '',
     sessionExpiredMsg: '',
-    methodType: '',
+    methodType: ''
   };
 
+  constructor(
+    private httpService: HttpServiceService,
+    private dataValidator: DataValidator,
+    private router: Router,
+    private cookieService: CookieService,
+    private route: ActivatedRoute,
+    private serviceLocator: ServiceLocatorService
+  ) { }
 
-  constructor(private httpService: HttpServiceService, private dataValidator: DataValidator, private router: Router,
-    private cookieService: CookieService, private route: ActivatedRoute, private serviceLocator: ServiceLocatorService) {
-
-  }
-
-  /**
-   * Initialize component
-   */
   ngOnInit() {
-    //this.userSessionCheck();
-    if(this.httpService.form.error == true){
+
+    if (this.httpService.form.error === true) {
       this.form.message = this.httpService.form.message;
       this.form.error = this.httpService.form.error;
     }
-    var a = '';
+
+    let a = '';
     this.serviceLocator.getPathVariable(this.route, function (params) {
       a = params["userparams"];
       console.log('I GOT ID, its logout', a);
-    })
-    if(a=='true'){
-       this.form.message = 'Logout Successfully';
-     }
-    }
-
-  userSessionCheck() {
-
-    this.route.paramMap.subscribe(params => {
-      //  this.userList = params.get('userparams');    
-      //  this.userparams=JSON.parse(this.route.snapshot.paramMap.get('userparams'));
-
-      this.userparams = JSON.parse(this.route.snapshot.queryParamMap.get('userparams'));
-      if (this.userparams != null) {
-        this.form.message = this.userparams.sessionExpiredMsg;
-        this.form.loginUrl = this.userparams.url;
-      //  console.log('URL-----------------------' + this.userparams.url);
-      //  console.log('Message-----------------------' + this.userparams.sessionExpiredMsg);
-      //  console.log('Method Type-----------------------' + this.userparams.methodType);
-      }
-
     });
-    if (this.form.message != null) {
-      this.form.error = true;
+
+    if (a === 'true') {
+      this.form.message = 'Logout Successfully';
     }
   }
 
   validate() {
     let flag = true;
     flag = flag && this.dataValidator.isNotNull(this.form.loginId);
-    console.log(this.form.loginId);
     flag = flag && this.dataValidator.isNotNull(this.form.password);
-    console.log(this.form.password);
     return flag;
   }
 
-
   signIn() {
-    
-    var _self = this;   
+
+    let _self = this;
+
     this.form.error = false;
-    const requestedUrl = this.httpService.userparams.url;//to get the URI
-    console.log('signIn----', this.form);
+
+    const requestedUrl = this.httpService.userparams.url;
+
     this.httpService.post(this.endpoint + "/login", this.form, function (res) {
 
       console.log('MyResponse', res);
 
-      
       _self.form.message = '';
-      _self.inputerror = {};
-      //_self.form.loginId = res.result.loginId;
+      _self.form.inputerror = {};       // ✅ Now part of form
+
       if (_self.dataValidator.isNotNullObject(res.result.message)) {
         _self.form.message = res.result.message;
-        console.log('messageeeeeeeeee',res.result.message);
       }
 
       _self.form.error = !res.success;
 
       if (_self.dataValidator.isNotNullObject(res.result.inputerror)) {
-        _self.inputerror = res.result.inputerror;
+        _self.form.inputerror = res.result.inputerror;   // ✅ Correct Mapping
       }
 
       if (_self.dataValidator.isTrue(res.success)) {
 
         _self.httpService.setToken(res.result.token);
         localStorage.setItem("loginId", res.result.loginId);
+
         let tokenStr = "Bearer " + res.result.token;
         localStorage.setItem("token", tokenStr);
         localStorage.setItem("role", res.result.role);
         localStorage.setItem("fname", res.result.fname);
         localStorage.setItem("lname", res.result.lname);
         localStorage.setItem("userid", res.result.data.id);
-     //   console.log(res.result.data.id + 'sessionId set ----');
-     //   console.log(res.result.token + '  Token set ----');
 
-
-        if (requestedUrl != null && requestedUrl != '') {
+        if (requestedUrl) {
           _self.router.navigateByUrl(requestedUrl);
-          
         } else {
           _self.router.navigateByUrl('dashboard');
         }
